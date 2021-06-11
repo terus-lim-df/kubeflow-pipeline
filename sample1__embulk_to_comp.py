da794cc4e58728b25a1ced61aecd6cd0f9363ff4
L-Thirat <83697210+terus-lim-df@users.noreply.github.com>
2021-06-11 23:30:21 +0900

    fix

--- sample1__embulk_to_comp.py
+++ sample1__embulk_to_comp.py
@@ -1,13 +1,14 @@
 import kfp
-import os
 
 
 # /home/minikf/embulk/./try1/csv/sample_
 def read_csv(i_file):
+    import os
     os.system('export i_file=' + i_file + '; embulk run /home/minikf/embulk/config.yml.liquid')
 
 
 create_step_merge_csv = kfp.components.create_component_from_func(
     func=read_csv,
     output_component_file='sample1__embulk_component.yaml',  # This is optional. It saves the component spec for future use.
-    base_image='python:3.7')
+    base_image='python:3.7',
+    packages_to_install=['os'])
